f33ebc262419564dbec8559825f468b0aa1313cb
ahnaf0731 <ahnafmnm01@gmail.com>
2025-08-23 04:39:53 +0530

    Add CI pipeline

--- src/test/java/com/fixitnow/selenium/LoginTest.java
+++ src/test/java/com/fixitnow/selenium/LoginTest.java
@@ -1,27 +1,27 @@
 package com.fixitnow.selenium;
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT) // starts app at localhost:8080
 public class LoginTest {
 
     @Test
     public void testLogin() {
-        // Set path to your ChromeDriver
         System.setProperty("webdriver.chrome.driver", "C:\\drivers\\chromedriver.exe");
 
         WebDriver driver = new ChromeDriver();
-        driver.get("http://localhost:8080/login"); // Replace with your app URL
+        driver.get("http://localhost:8080/login");
 
-        // You can add Selenium steps here, e.g., fill username/password and submit
-        // Example:
+        // Example login actions (uncomment after you have elements)
         // driver.findElement(By.id("username")).sendKeys("admin");
         // driver.findElement(By.id("password")).sendKeys("1234");
         // driver.findElement(By.id("loginButton")).click();
 
         System.out.println("Login test ran successfully!");
-
-        driver.quit();
+        //driver.quit();
     }
 }
